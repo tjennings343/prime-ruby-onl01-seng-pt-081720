f713f517a210b6127c931ebc1e5c737776ecd70e
Trey Jennings <tjennings343@gmail.com>
2020-08-28 00:29:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 def prime?(num)
-  if num > 1
+  if num > 2
     number_range = (2..num-1).to_a
     number_range.none? do |test_num|
       number % test_num == 0
-      true
     end
   else
     false
