423e590683896c963d4b9d4e12453a6534347307
Trey Jennings <tjennings343@gmail.com>
2020-08-28 00:30:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  if num >= 2
+    number_range = (2..num-1).to_a
+    number_range.none? do |test_num|
+      num % test_num == 0
+    end
+  else
+    false
+  end
+
+end
